e55f2c6e20a48ec4c1887965cb175f469a58be63
Francisco Santos <fsantos.fs37@gmail.com>
2020-06-24 15:14:57 -0300

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
+  console.log("env:,", process.env.REACT_APP_FIREBASE_CONFIG);
+
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,5 @@
 import firebase from "firebase";
 
-console.log(process.env.REACT_APP_FIREBASE_CONFIG);
-
 const appConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 
 firebase.initializeApp(appConfig);
