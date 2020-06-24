e782f3a9116c4a9bc5f6bf030c48b869f16a164a
Francisco Santos <fsantos.fs37@gmail.com>
2020-06-24 15:11:30 -0300

    teste

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import firebase from "firebase";
 
+console.log(process.env.REACT_APP_FIREBASE_CONFIG);
+
 const appConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 
 firebase.initializeApp(appConfig);
