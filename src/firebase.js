03ce9402e246987b13b96e1911b556d3e07fabd8
Francisco Santos <fsantos.fs37@gmail.com>
2020-06-24 16:30:19 -0300

    test

--- src/firebase.js
+++ src/firebase.js
@@ -2,8 +2,16 @@ import firebase from "firebase";
 
 const appConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 
-console.log(appConfig);
-
 firebase.initializeApp(appConfig);
 
 export const db = firebase.firestore();
+
+export const init = () => {
+  db.collection("users")
+    .doc("test")
+    .get()
+    .then((res) => {
+      console.log(res.data());
+    })
+    .catch((err) => console.log({ err }));
+};
